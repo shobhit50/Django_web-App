bd3b8692643d8fe2fe0e8cbd13bd6711c5d267eb
Shobhit Kumar Parashar <91795621+shobhit50@users.noreply.github.com>
2024-01-06 14:11:13 +0530

    Update settings.py

--- wunderlust/settings.py
+++ wunderlust/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-eiulkq204d&2h^l)*a6q*!3gc2*v#0)*r%q03%tf&mbzf6qc6$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wunderlust-django.onrender.com']
 
 AUTH_USER_MODEL = 'myfirst_app.userdata'
 
