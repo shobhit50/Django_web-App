2ae2395be02fcc928b2b9d2889ab50cafd894ece
Shobhit kumar parashar <shobhitparashar0@gmail.com>
2024-01-06 16:23:13 +0530

    testing5

--- wunderlust/settings.py
+++ wunderlust/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -95,17 +95,17 @@ DATABASES = {
             'NAME': 'Django',
             'ENFORCE_SCHEMA': False,
             'CLIENT': {
-                'host': f'mongodb+srv://shobhit:{os.environ["DB_PASS"]}@cluster0.snn3wbn.mongodb.net/Django?retryWrites=true&w=majority'
+                # 'host': f'mongodb+srv://shobhit:{os.environ["DB_PASS"]}@cluster0.snn3wbn.mongodb.net/Django?retryWrites=true&w=majority'
             }  
         }
 }
 
 
-CLOUDINARY = {
-    'cloud_name': {os.environ['CLOUD_NAME']},
-    'api_key': {os.environ['API_KEY']},
-    'api_secret': {os.environ['API_SECRET']},
-}
+# CLOUDINARY = {
+#     'cloud_name': {os.environ['CLOUD_NAME']},
+#     'api_key': {os.environ['API_KEY']},
+#     'api_secret': {os.environ['API_SECRET']},
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR.parent, 'static'),
+    STATIC_DIR,
 ]
 
 # Default primary key field type
