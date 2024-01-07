2fd1829b3afd6cd8e98a94868b7f81780123156c
shiv <shivkumar0757@gmail.com>
2024-01-07 10:20:33 +0530

    Remove commented out code in settings.py and Initialise readme file

--- wunderlust/settings.py
+++ wunderlust/settings.py
@@ -6,10 +6,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-# STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
