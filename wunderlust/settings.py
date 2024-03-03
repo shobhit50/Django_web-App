0d0211c581ce66fcd2d71b0f8002121f19ad075e
Shobhit kumar parashar <shobhitparashar0@gmail.com>
2024-03-03 13:23:34 +0530

    fix

--- wunderlust/settings.py
+++ wunderlust/settings.py
@@ -140,7 +140,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
